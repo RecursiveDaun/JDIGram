c49d33b654149198e61fd839d613b16d4bf7617b
RecursiveDaun <argonianinn@gmail.com>
2020-05-04 20:37:38 +0300

    Added "Welcome" controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' } do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  root :to => 'welcome#index'
 end
