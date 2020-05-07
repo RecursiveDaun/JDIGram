8cbeb439b2fa9c0a52c217ed9114dbda1d69e88f
RecursiveDaun <argonianinn@gmail.com>
2020-05-07 19:27:13 +0300

    added active storage configuration for production

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -9,7 +9,6 @@ class ProfileController < ApplicationController
   end
 
   def update
-    p '===================== Profile data ====================='
     profile_data = params[:user_profile]
     p '===================== Update Attributes ====================='
     @profile.update_attributes(:name => profile_data[:name], :age => profile_data[:age])
@@ -26,12 +25,12 @@ class ProfileController < ApplicationController
   end
 
   def allowed_params
-    params.require(:user_profile).permit(:age, :name, :avatar)
+    params.require(:user_profile).permit(:age, :name)#, :avatar)
   end
 
-  def photo_params
-    params.require(:user_profile).require(:data)
-  end
+  # def photo_params
+  #   params.require(:user_profile).require(:data)
+  # end
 
   # private
   # def create_photo
@@ -46,14 +45,14 @@ class ProfileController < ApplicationController
   private
 
   def find_profile_by_params
-    p '===================== Save Condition ====================='
-    p params[:id], " ", is_id_number?(params[:id])
     if is_id_number?(params[:id])
       p '===================== is_id_number? true ====================='
       @profile = User.find(params[:id]).user_profile
+      p @profile
     else
       p '===================== is_id_number? false ====================='
       @profile = User.where('link_hash = ?', params[:id]).first.user_profile
+      p @profile
     end
   end
 
