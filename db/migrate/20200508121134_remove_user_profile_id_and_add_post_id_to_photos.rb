a3b79f45c5035f83e7b06d12950aaa7a0098513d
RecursiveDaun <argonianinn@gmail.com>
2020-05-08 19:07:57 +0300

    Apply 3 rollback for merge to master branch

--- db/migrate/20200508121134_remove_user_profile_id_and_add_post_id_to_photos.rb
+++ db/migrate/20200508121134_remove_user_profile_id_and_add_post_id_to_photos.rb
@@ -1,6 +1,6 @@
 class RemoveUserProfileIdAndAddPostIdToPhotos < ActiveRecord::Migration[5.2]
   def change
     add_column :photos, :post_id, :integer
-    remove_column :photos, :user_profile_id
+    remove_column :photos, :user_profile_id, :integer
   end
 end
--- db/migrate/20200508151838_remove_photo_id_from_posts.rb
+++ db/migrate/20200508151838_remove_photo_id_from_posts.rb
@@ -1,5 +1,5 @@
 class RemovePhotoIdFromPosts < ActiveRecord::Migration[5.2]
   def change
-    remove_column :posts, :photo_id
+    remove_column :posts, :photo_id, :integer
   end
 end
