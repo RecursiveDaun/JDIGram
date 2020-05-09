d0c11c027ddc82625b04cd37efba6b118dbf7267
RecursiveDaun <argonianinn@gmail.com>
2020-05-10 01:48:49 +0300

    Added placeholders to empty profile fields, fixed design bugs and code duplicates

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,7 +1,3 @@
-$(document).ready(function() {
-    console.log('qwertyy')
-});
-
 function show_post_image_preview(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -9,8 +5,6 @@ function show_post_image_preview(input) {
         reader.onload = function (e) {
             $('#image_preview').attr('src', e.target.result);
             $('#image_preview').css("width", "100%", "height", "100%");
-            console.log('ds')
-            console.log(input.files[0].name)
             $('#post_image_label').text(input.files[0].name);
         };
 
