3ca6722cc7784c743584c7f792b07e9e9c980ce9
RecursiveDaun <argonianinn@gmail.com>
2020-05-14 09:58:46 +0300

    configured elasticsearch in config/environment/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.active_storage.service = :google
+
+  Elasticsearch::Model.user = Elasticsearch::User.new host: ENV['SEARCHBOX_URL']
 end
