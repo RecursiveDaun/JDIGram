5758e153e71d0b41f0c20e947f743ed4e4d636f5
RecursiveDaun <argonianinn@gmail.com>
2020-05-15 06:25:55 +0300

    Fixed bug with empty search template by search bar

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 
   def search
     if params['search-keyword'].blank?
+      redirect_to welcome_index_path
       return
     end
     @users = User.search(params['search-keyword'])
