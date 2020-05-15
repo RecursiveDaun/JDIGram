4d8067d88e33cfbe313a118b0a63dd5a5909d1f0
RecursiveDaun <argonianinn@gmail.com>
2020-05-15 13:05:22 +0300

    fixed bugs for conversation views, added functional for create, show one and display all conversations

--- app/controllers/conversation_controller.rb
+++ app/controllers/conversation_controller.rb
@@ -1,5 +1,7 @@
 class ConversationController < ApplicationController
 
+  before_action :find_conversation_by_params, only: [:show]
+
   def index
     @conversations = Conversation.where('author_id = ? OR receiver_id = ?',
                                         current_user.user_profile.id,
@@ -7,15 +9,33 @@ class ConversationController < ApplicationController
   end
 
   def show
-    p 'into conversation show'
-    # @conversation = Conversation.find(params[:id])
-    render plain: 'Hello'
   end
 
   def create
-    conversation = Conversation.new
-    conversation.update_attributes(author_id: current_user.user_profile.id, receiver_id: params[:receiver_id])
-    conversation.save!
+    conversation = Conversation.where('author_id IN (?, ?) AND receiver_id IN (?, ?)',
+                                      current_user.user_profile.id,
+                                      params[:format],
+                                      current_user.user_profile.id,
+                                      params[:format]).first
+    if conversation.blank?
+      conversation = Conversation.new
+      conversation.update_attributes(author_id: current_user.user_profile.id, receiver_id: fetch_receiver_id_from_params)
+      conversation.save
+    end
+
+    redirect_to conversation_path(conversation.id)
+  end
+
+
+  private
+
+  def find_conversation_by_params
+    conv_id = params[:id] || params[:format]
+    @conversation = Conversation.find(conv_id)
+  end
+
+  def fetch_receiver_id_from_params
+    params[:id] || params[:format]
   end
 
 end
