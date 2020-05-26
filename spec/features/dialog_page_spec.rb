76dde4c4f0a10de53c74326f17fef5119ce0c502
RecursiveDaun <argonianinn@gmail.com>
2020-05-26 14:44:59 +0300

    Added test for the message page

---
+++ spec/features/dialog_page_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe "Dialog page", type: :feature, js: true do
+
+  let(:current_user) { User.where(nickname: 'first').first }
+  let(:conversation) { Conversation.where('author_id = ? OR receiver_id = ?', current_user.profile.id, current_user.profile.id).first }
+
+  it "should contain messages" do
+    visit '/'
+    # Sign in with existing user
+    fill_in :user_login, with: current_user.nickname
+    fill_in :user_password, with: '123123'
+    click_button 'Sign in'
+
+    visit conversation_path(conversation)
+
+    expect(page).to have_css(".messages-column")
+    expect(page).to have_css(".conversation-companion-name")
+    expect(page).to have_css("#messages_#{conversation.id}")
+
+    if conversation.messages.count > 0
+      expect(page).to have_css(".button_avatar_link_to_profile")
+      expect(page).to have_css(".avatar_link_to_profile")
+      expect(page).to have_css(".message-author")
+      expect(page).to have_css(".message-time")
+      expect(page).to have_css(".message-body")
+    else
+      expect(page).to have_no_css(".button_avatar_link_to_profile")
+      expect(page).to have_no_css(".avatar_link_to_profile")
+      expect(page).to have_no_css(".message-author")
+      expect(page).to have_no_css(".message-time")
+      expect(page).to have_no_css(".message-body")
+    end
+
+  end
+
+end
