37b871a9a31e72960c16219af6d592b4fad213c8
RecursiveDaun <argonianinn@gmail.com>
2020-05-26 17:22:13 +0300

    Added test for edit user profile page

--- spec/features/current_user_profile_spec.rb
+++ spec/features/current_user_profile_spec.rb
@@ -26,10 +26,20 @@ describe "Current user profile page", type: :feature, js: true do
       expect(page).to have_css(".dark-horizontal-separator")
       expect(page).to have_content("#{profile.name} posts".upcase)
       expect(page).to have_css(".user-posts-grid-column")
-
       expect(page).to have_css("img.user-profile-post-image")
+    else
+      expect(page).to have_no_css(".dark-horizontal-separator")
+      expect(page).to have_no_content("#{profile.name} posts".upcase)
+      expect(page).to have_no_css(".user-posts-grid-column")
+      expect(page).to have_no_css("img.user-profile-post-image")
     end
 
+    # Test edit profile
+    click_on("Edit profile")
+    expect(page).to have_content("Edit your profile")
+    expect(page).to have_field('user_profile_name', with: "#{profile.name}")
+    expect(page).to have_field('user_profile_age', with: "#{profile.age}")
+    expect(page).to have_css("#profile_avatar_label")
   end
-
+  
 end
