b5f52a3248e4630ab9ae0333eb8519bcb531354e
RecursiveDaun <argonianinn@gmail.com>
2020-05-27 11:31:13 +0300

    Small design behaviours update

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -6,8 +6,8 @@ function show_post_image_preview(input) {
             $('#image_preview').attr('src', e.target.result);
             $('#image_preview').css("width", "100%", "height", "100%");
             $('#post_image_label').text(input.files[0].name);
+            window.scrollTo(0, document.body.scrollHeight);
         };
-
         reader.readAsDataURL(input.files[0]);
     }
 }
