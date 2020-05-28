b8795c6ab727d338d89119e57cd136e53be0ca5d
RecursiveDaun <argonianinn@gmail.com>
2020-05-28 13:29:37 +0300

    Fixed bugs with the search result page, added validation for unique before we will display are results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,20 @@ class SearchController < ApplicationController
       redirect_to welcome_index_path
       return
     end
-    @users = User.search(params['search-keyword'])
-    @user_profiles = UserProfile.search(params['search-keyword'])
+
+    @users_profiles = [ActiveRecord]
+    elastic_users = User.search(params['search-keyword'])
+    elastic_users.each do |user|
+      @users_profiles.push(User.find(user.id).profile)
+    end
+    @users_profiles.delete_at(0)
+
+    elastic_user_profiles = UserProfile.search(params['search-keyword'])
+    elastic_user_profiles.each do |profile|
+      @users_profiles.push(UserProfile.find(profile.id))
+    end
+
+    @users_profiles = @users_profiles.uniq { |profile| profile.id }
     render :search_results
   end
 
